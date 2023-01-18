0cf38292abd7e186207b5de9e63b8816b601ead2
Sunil Ballani <ballanisunil123@gmail.com>
2023-01-18 22:58:53 +0530

    refactor: added data in dashboard

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -1,34 +1,55 @@
 <template>
-  <div class="dashboard ml-2">
+  <div class="dashboard ml-4">
     <h1 class="subheading grey--text">Dashboard</h1>
 
-    <v-container class="my-5"> 
-      <v-row wrap>
+    <v-container class="my-5">
+      <v-card flat class="pa-3 mb-2" v-for="project in projects" :key="project.title">
+        <v-row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
-            <v-btn outline block class="primary my-1">1</v-btn>
+            <div class="caption grey--text">Project title</div>
+            <div>{{project.title}}</div>
           </v-flex>
-          <v-flex xs4 md2>
-            <v-btn outline block class="primary my-1">2</v-btn>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>{{project.person}}</div>
           </v-flex>
-          <v-flex xs4 md2>
-            <v-btn outline block class="primary my-1">2</v-btn>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+            <div>{{project.due}}</div>
           </v-flex>
-          <v-flex xs4 md2>
-            <v-btn outline block class="primary my-1">2</v-btn>
+          <v-flex xs3 sm4 md2>
+            <div class="caption grey--text">Status</div>
+            <div>{{project.status}}</div>
           </v-flex>
-      </v-row> 
-      <v-row wrap class="justify-space-around">
-        <v-flex xs4 md3>
-          <v-btn outline block class="success my-1">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-          <v-btn outline block class="success my-1">2</v-btn>
-        </v-flex>
-      </v-row>
+        </v-row>
+      </v-card>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return{
+      projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    }
+  }
+};
 </script>
+
+<style>
+.project.complete{
+  border-left: 4px solid #3cd1cd;
+}
+.project.ongoing{
+  border-left: 4px solid orange;
+}
+.project.overdue{
+  border-left: 4px solid tomato;
+}
+</style>
