dd053091c114c29d669aef44031420394212aa2a
Sunil Ballani <ballanisunil123@gmail.com>
2023-01-19 20:30:16 +0530

    feat: added menu button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,6 +8,26 @@
         <span>Manager</span>
       </v-app-bar-title>
       <v-spacer></v-spacer>
+
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on" color="grey">
+            <v-icon left>expand_more</v-icon>
+            <span>menu</span>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="link in links"
+            :key="link.text"
+            router
+            :to="link.route"
+          >
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
       <v-btn text color="grey">
         <span>Sign Out</span>
         <v-icon right>exit_to_app</v-icon>
@@ -18,25 +38,29 @@
       <v-col align-center>
         <v-flex class="mt-5">
           <v-avatar size="100">
-            <img src="/avatar-1.png" alt="">
+            <img src="/avatar-1.png" alt="" />
           </v-avatar>
-          <p class="white--text subheading mt-1">
-            The Net Ninja
-          </p>
+          <p class="white--text subheading mt-1">The Net Ninja</p>
         </v-flex>
       </v-col>
       <v-list dense nav>
-        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+        >
           <v-list-item-icon>
-            <v-icon class="white--text">{{link.icon}}</v-icon>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-tile-title class="white--text">{{link.text}}</v-list-tile-title>
+            <v-list-tile-title class="white--text">{{
+              link.text
+            }}</v-list-tile-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-
   </nav>
 </template>
 
@@ -46,10 +70,10 @@ export default {
     return {
       drawer: false,
       links: [
-        {icon: 'dashboard', text: 'Dashboard', route: '/'},
-        {icon: 'folder', text: 'My Projects', route: '/projects'},
-        {icon: 'person', text: 'Team', route: '/team'}
-      ]
+        { icon: "dashboard", text: "Dashboard", route: "/" },
+        { icon: "folder", text: "My Projects", route: "/projects" },
+        { icon: "person", text: "Team", route: "/team" },
+      ],
     };
   },
 };
